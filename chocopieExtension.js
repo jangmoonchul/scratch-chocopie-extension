0b5c41671f872748443f46ea805ac3b758ac66d0
remoted <35mwlee@naver.com>
2016-04-09 03:54:28 +0900

    20160409 bug modify 2

--- chocopieExtension.js
+++ chocopieExtension.js
@@ -198,11 +198,25 @@
             ['h', 'when %m.booleanSensor',         'whenSensorConnected', 'button pressed'],
             ['h', 'when %m.sensor %m.lessMore %n', 'whenSensorPass',      'slider', '>', 50],
             ['b', 'sensor %m.booleanSensor?',      'sensorPressed',       'button pressed'],
-            ['r', '%m.sensor sensor value',        'sensor',              'Analog1']
+            ['r', '%m.sensor sensor value',        'sensor',              'Analog1'],
+			['r', '%m.touch touch-sensor value',   'touch',               '1'],
+			['r', '%m.sw button value',			   'sw',                  '1'],
+			['r', '%m.joystick joystick value',    'joystick',            'X'],
+			['r', '%m.potency potencyometer value','potency',             '1'],
+			['r', '%m.infrared infrared value',    'infrared',            '1'],
+			['r', '%m.acceler acceler value',      'acceler',             'X'],
+			['r', '%m.pacceler acceler value',    'p-acceler',           'U']
         ],
         menus: {
             booleanSensor: ['button pressed', 'A connected', 'B connected', 'C connected', 'D connected'],
             sensor: ['Analog1', 'Analog2', 'Analog3', 'Analog4', 'lux', 'temperature', 'humidity'],
+			touch: ['1', '2', '3', '4', '5', '6', '7','8','9','10','11','12'],
+			sw: ['1', '2', '3', '4', 'J'],
+			joystick: ['X', 'Y'],
+			potency: ['1'],
+			infrared: ['1','2','3'],
+			acceler: ['X','Y','Z'],
+			pacceler: ['U','V','W'],
             lessMore: ['>', '<']
         },
         url: '/info/help/studio/tips/ext/PicoBoard/'
