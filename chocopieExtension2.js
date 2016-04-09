93f75c63cda9a502010089c674b3e6808fa68c4d
remoted <35mwlee@naver.com>
2016-04-10 06:58:37 +0900

    LED Block is defined

--- chocopieExtension2.js
+++ chocopieExtension2.js
@@ -585,12 +585,16 @@
 	  ['r', 'read infrared value is %m.infrared', 'infraredRead', '1'],
 	  ['r', 'read acceler value is %m.acceler', 'accelerRead', 'X'],
 	  ['r', 'read pacceler value is %m.pacceler', 'paccelerRead', 'U'],
-	  ['r', 'read remoted infrared value is %m.Rinfrared', 'RinfraredRead', '1'],
-	  ['r', 'read remoted acceler value is %m.Racceler', 'RaccelerRead', 'X'],
-	  ['r', 'read remoted pacceler value is %m.Rpacceler', 'RpaccelerRead', 'U'],
+	  ['r', 'read remoted infrared value is %m.infrared', 'RinfraredRead', '1'],
+	  ['r', 'read remoted acceler value is %m.acceler', 'RaccelerRead', 'X'],
+	  ['r', 'read remoted pacceler value is %m.pacceler', 'RpaccelerRead', 'U'],
 	  ['-'],
-	  ['h', 'When %m.photoGate is %m.gateState', 'whenPhoto', '1', 'blocked'],
-	  ['r', 'read %m.photoGate value', 'photoRead', '1']
+	  ['h', 'When photogate %m.photoGate is %m.gateState', 'whenPhoto', '1', 'blocked'],
+	  ['r', 'read photogate %m.photoGate value', 'photoRead', '1'],
+	  ['h', 'When remoted photogate %m.photoGate is %m.gateState', 'whenRPhoto', '1', 'blocked'],
+	  ['r', 'read remoted photogate %m.photoGate value', 'RphotoRead', '1'],
+	  ['-'],
+	  [' ', 'RED %n GREEN %n BLUE %n', 'passLEDrgb', '0', '0', '0']
     ],
     ko: [
       ['h', '초코파이가 연결됐을 때', 'whenConnected'],
@@ -632,12 +636,16 @@
 	  ['r', '적외선 %m.infrared 의 값', 'infraredRead', '1'],		//Infrared, acceler and personal acceler is repoter block.
 	  ['r', '가속도 %m.acceler 의 값', 'accelerRead', 'X'],			//function_name : infraredRead	accelerRead	paccelerRead
 	  ['r', '각 가속도%m.pacceler 의 값', 'paccelerRead', 'U'],
-	  ['r', '적외선 %m.Rinfrared 의 값', 'RinfraredRead', '1'],		//Infrared, acceler and personal acceler is repoter block.
-	  ['r', '가속도 %m.Racceler 의 값', 'RaccelerRead', 'X'],			//function_name : RinfraredRead	RaccelerRead	RpaccelerRead
-	  ['r', '각 가속도%m.Rpacceler 의 값', 'RpaccelerRead', 'U'],
+	  ['r', '적외선 %m.infrared 의 값', 'RinfraredRead', '1'],		//Infrared, acceler and personal acceler is repoter block.
+	  ['r', '가속도 %m.acceler 의 값', 'RaccelerRead', 'X'],			//function_name : RinfraredRead	RaccelerRead	RpaccelerRead
+	  ['r', '각 가속도%m.pacceler 의 값', 'RpaccelerRead', 'U'],
 	  ['-'],
-	  ['h', '%m.photoGate 가 %m.gateState', 'whenPhoto', '1', '막히면'],	//Photogate and gatestate is defined.
-	  ['r', '%m.photoGate 의 값', 'photoRead', '1']							//function_name : whenPhoto	photoRead
+	  ['h', '포토게이트 %m.photoGate 가 %m.gateState', 'whenPhoto', '1', '막히면'],	//Photogate and gatestate is defined.
+	  ['r', '포토게이트 %m.photoGate 의 값', 'photoRead', '1'],							//function_name : whenPhoto	photoRead
+	  ['h', '원격 포토게이트 %m.photoGate 가 %m.gateState', 'whenRPhoto', '1', '막히면'],	//Remote Photogate and remote gatestate is defined.
+	  ['r', '원격 포토게이트 %m.photoGate 의 값', 'RphotoRead', '1'],						//function_name : whenRPhoto	RphotoRead
+	  ['-'],														//LED RGB definition
+	  [' ', '빨강 %n 녹색 %n 파랑 %n', 'passLEDrgb', '0', '0', '0']	//function_name : passLEDrgb
     ]
   };
 
@@ -650,7 +658,32 @@
 
 		hwOut: ['led A', 'led B', 'led C', 'led D', 'button A', 'button B', 'button C', 'button D', 'servo A', 'servo B', 'servo C', 'servo D'], 
 		//To out Hardware Value 
-		leds: ['led A', 'led B', 'led C', 'led D'],
+		leds: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
+			'11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
+			'21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
+			'31', '32', '33', '34', '35', '36', '37', '38', '39', '40',
+			'41', '42', '43', '44', '45', '46', '47', '48', '49', '50',
+			'51', '52', '53', '54', '55', '56', '57', '58', '59', '60',
+			'61', '62', '63', '64', '65', '66', '67', '68', '69', '70',
+			'71', '72', '73', '74', '75', '76', '77', '78', '79', '80',
+			'81', '82', '83', '84', '85', '86', '87', '88', '89', '90',
+			'91', '92', '93', '94', '95', '96', '97', '98', '99', '100',
+			'101', '102', '103', '104', '105', '106', '107', '108', '109', '110',
+			'111', '112', '113', '114', '115', '116', '117', '118', '119', '120',
+			'121', '122', '123', '124', '125', '126', '127', '128', '129', '130',
+			'131', '132', '133', '134', '135', '136', '137', '138', '139', '140',
+			'141', '142', '143', '144', '145', '146', '147', '148', '149', '150',
+			'151', '152', '153', '154', '155', '156', '157', '158', '159', '160',
+			'161', '162', '163', '164', '165', '166', '167', '168', '169', '170',
+			'171', '172', '173', '174', '175', '176', '177', '178', '179', '180',
+			'181', '182', '183', '184', '185', '186', '187', '188', '189', '190',
+			'191', '192', '193', '194', '195', '196', '197', '198', '199', '200',
+			'201', '202', '203', '204', '205', '206', '207', '208', '209', '210',
+			'211', '212', '213', '214', '215', '216', '217', '218', '219', '220',
+			'221', '222', '223', '224', '225', '226', '227', '228', '229', '230',
+			'231', '232', '233', '234', '235', '236', '237', '238', '239', '240',
+			'241', '242', '243', '244', '245', '246', '247', '248', '249', '250',
+			'251', '252', '253', '254', '255'],
 		outputs: ['on', 'off'],
 		ops: ['>', '=', '<'],
 		servos: ['servo A', 'servo B', 'servo C', 'servo D'],
@@ -687,7 +720,32 @@
 		RhwIn: ['원격 조도 센서', '원격 온도 센서','원격 습도 센서'], 
 
 		hwOut: ['led A', 'led B', 'led C', 'led D', '버튼 A', '버튼 B', '버튼 C', '버튼 D', '서보모터 A', '서보모터 B', '서보모터 C', '서보모터 D'],
-		leds: ['led A', 'led B', 'led C', 'led D'],
+		leds: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
+			'11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
+			'21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
+			'31', '32', '33', '34', '35', '36', '37', '38', '39', '40',
+			'41', '42', '43', '44', '45', '46', '47', '48', '49', '50',
+			'51', '52', '53', '54', '55', '56', '57', '58', '59', '60',
+			'61', '62', '63', '64', '65', '66', '67', '68', '69', '70',
+			'71', '72', '73', '74', '75', '76', '77', '78', '79', '80',
+			'81', '82', '83', '84', '85', '86', '87', '88', '89', '90',
+			'91', '92', '93', '94', '95', '96', '97', '98', '99', '100',
+			'101', '102', '103', '104', '105', '106', '107', '108', '109', '110',
+			'111', '112', '113', '114', '115', '116', '117', '118', '119', '120',
+			'121', '122', '123', '124', '125', '126', '127', '128', '129', '130',
+			'131', '132', '133', '134', '135', '136', '137', '138', '139', '140',
+			'141', '142', '143', '144', '145', '146', '147', '148', '149', '150',
+			'151', '152', '153', '154', '155', '156', '157', '158', '159', '160',
+			'161', '162', '163', '164', '165', '166', '167', '168', '169', '170',
+			'171', '172', '173', '174', '175', '176', '177', '178', '179', '180',
+			'181', '182', '183', '184', '185', '186', '187', '188', '189', '190',
+			'191', '192', '193', '194', '195', '196', '197', '198', '199', '200',
+			'201', '202', '203', '204', '205', '206', '207', '208', '209', '210',
+			'211', '212', '213', '214', '215', '216', '217', '218', '219', '220',
+			'221', '222', '223', '224', '225', '226', '227', '228', '229', '230',
+			'231', '232', '233', '234', '235', '236', '237', '238', '239', '240',
+			'241', '242', '243', '244', '245', '246', '247', '248', '249', '250',
+			'251', '252', '253', '254', '255'],
 		outputs: ['켜기', '끄기'],
 		ops: ['>', '=', '<'],
 		servos: ['서보모터 A', '서보모터 B', '서보모터 C', '서보모터 D'],
