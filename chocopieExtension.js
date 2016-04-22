3f37303c86224aa2fbcb4d41c1d6546594376e62
remoted <35mwlee@naver.com>
2016-04-23 00:14:41 +0900

    chocopieExtension Init refactoring ping bug patch 17

--- chocopieExtension.js
+++ chocopieExtension.js
@@ -295,10 +295,13 @@
           processSysexMessage();
 		  //예상값) storedInputData[0] = 0xE0 혹은 0xF0
         }else{
-			if (i < 11){		
+			if (i < 11){
 				storedInputData[sysexBytesRead++] = inputData[i];				//detail/port + Data ( 10 Byte) = 11 Byte 이상이면 강제로 끊어버림 
 				console.log('storedInputData [' + sysexBytesRead + '] ' + storedInputData[sysexBytesRead]);				
+			}else{
+				storedInputData[sysexBytesRead] = inputData[i];
 			}
+
         }
 			
       } else if ( waitForData > 0 && ( (inputData[0] >= 0xE0 && inputData[0] <= 0xE2) || (inputData[0] >= 0xF0 && inputData[0] <= 0xF2) ) && inputData[1] <= 0x0F ){					
