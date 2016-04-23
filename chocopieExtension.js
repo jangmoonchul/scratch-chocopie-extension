82a8560db025cc1bbd3d9c411aa8a9ebf0ab0bb1
remoted <35mwlee@naver.com>
2016-04-23 21:51:22 +0900

    chocopieExtension compare CPC_VERSION and CPC_GET_BLOCK logging3

--- chocopieExtension.js
+++ chocopieExtension.js
@@ -375,7 +375,7 @@
           }
 		}	
       } else {
-        if ((inputData[0] == 0xE0 || inputData[0] == 0xF0)  && inputData[1] == CPC_VERSION ) {	//0xE0 인 경우, 초코파이보드 확정과정에서만 쓰임
+        if ((inputData[0] == 0xE0 || inputData[0] == 0xF0)  && (inputData[1] == CPC_VERSION || storedInputData[1] === CPC_VERSION) ) {	//0xE0 인 경우, 초코파이보드 확정과정에서만 쓰임
 			detail = inputData[i];	//예상 데이터) 0xE0, CPC_VERSION, “CHOCOPI”,1,0...
 									//들어온 데이터를 분석해서 상위 4비트에 대해서는 command 로, 하위 4비트에 대해서는 multiByteChannel로 사용
 									//일반적으로는 [1] 스택에 대하여 데이터가 리스팅되지만, CPC_VERSION 이나 GET_BLOCK 의 경우는 SYSTEM 명령어로써 데이터가옴
@@ -386,7 +386,7 @@
           multiByteChannel = inputData[0] & 0x0F;						// -> hwList.search_bypin 로 조사해서 처리해야함
 		  port = hwList.search_bypin(multiByteChannel);
         }
-		if((detail === SCBD_CHOCOPI_USB || detail === SCBD_CHOCOPI_BLE) && inputData[1] == CPC_VERSION){
+		if((detail === SCBD_CHOCOPI_USB || detail === SCBD_CHOCOPI_BLE) && (inputData[1] == CPC_VERSION || storedInputData[1] === CPC_VERSION)){
 			parsingSysex = true;
 			sysexBytesRead = 0;
 			console.log('detail parsing success and parsingSysex running');
