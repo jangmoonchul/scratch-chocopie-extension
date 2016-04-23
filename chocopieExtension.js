017ec9561bd078d9f113dccce5173c3350c8b2b2
remoted <35mwlee@naver.com>
2016-04-23 16:12:04 +0900

    chocopieExtension Init refactoring ping bug patch 34

--- chocopieExtension.js
+++ chocopieExtension.js
@@ -229,7 +229,7 @@
           connected = true;
 
 		  device.send(output_block.buffer);	
-          //setTimeout(init, 200);		
+          setTimeout(init, 200);		
 		  console.log('I send cpc_get_block');
 		  /* Connection 처리가 완료되었으므로, 이 곳에서 CPC_GET_BLOCK 에 대한 처리를 하는게 맞음 (1차 확인) -> (2차 확인 필요) */		
         }
@@ -289,8 +289,8 @@
 	  //입력 데이터 처리용도의 함수
     for (var i=0; i < inputData.length; i++) {	//i는 0부터 시작하지만, 결국적으로 1이 되서야  inputData[i] 를 storedInputData 에 담기 시작할 것임
       if (parsingSysex) {
-		if ((inputData[0] == SCBD_CHOCOPI_USB || inputData[0] == SCBD_CHOCOPI_BLE) && sysexBytesRead === 11) { 
-		  console.log('I am comming parsingSysex if');
+		if ((inputData[0] == SCBD_CHOCOPI_USB || inputData[0] == SCBD_CHOCOPI_BLE) && i === inputData.length) { 
+		  console.log('I am comming parsingSysex if');				// sysexBytesRead === 11
           parsingSysex = false;
           processSysexMessage();
 		  break;
