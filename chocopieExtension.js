0ac45957ef7791da67770588641c5706be275f12
remoted <35mwlee@naver.com>
2016-04-23 14:11:58 +0900

    chocopieExtension Init refactoring ping bug patch 22

--- chocopieExtension.js
+++ chocopieExtension.js
@@ -387,8 +387,6 @@
 		if((detail === SCBD_CHOCOPI_USB || detail === SCBD_CHOCOPI_BLE) && pingCount < 6 ){
 			parsingSysex = true;
 			sysexBytesRead = 0;
-			if (sysexBytesRead === 11)
-				sysexBytesRead = 0;
 			if ( i < 11 )
 				storedInputData[sysexBytesRead++] = detail;					// 0 부터 도는 for 문에 대해서 port/detail 을 놓치지 않기 위한 조치
 
