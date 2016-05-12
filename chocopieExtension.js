f9c6ba223600bade4511a5e851ebbef88ea7daf3
remoted <35mwlee@naver.com>
2016-05-13 05:09:51 +0900

    ChocopiExtension ALL BLOCK IS DEFINED 5

--- chocopieExtension.js
+++ chocopieExtension.js
@@ -646,6 +646,7 @@
 			if (port < 8) s.servo_block_usb[s.servo_count_usb] = port;		//만약 "servo" 에 이미 데이터가 존재하는 경우에는 port가 덮어씌워질 듯 하다
 			else s.servo_block_ble[s.servo_count_ble] = port;					//패치 완료
 			console.log("s.servo_count_usb " + s.servo_count_usb);
+			s.servo_count_usb++;
 		}
 	}
 	function nullBlock(){
